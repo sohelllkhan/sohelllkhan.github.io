a97d46da083fc430ee004c368bea6a20d302519e
Sohel Khan <sohelkhan323526@gmail.com>
2025-07-21 15:12:33 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,9 @@
 <?php
+// Allow POST from external sources (like GitHub Pages)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
   $email = $_POST["email"];
